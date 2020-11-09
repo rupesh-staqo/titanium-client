53524b0d89a1f316a3829a09ca495cf5365c57d4
Rupesh Ranjan <rupesh.ranjan@staqo.com>
2020-11-09 08:25:16 +0530

    Rupesh :: added company id

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://3.96.249.114/autotrader_csv_import/server/api/";
-export const API_URL = "http://localhost/titanium/server/api/";
+export const API_URL = "http://3.96.249.114/autotrader_csv_import/server/api/";
+// export const API_URL = "http://localhost/titanium/server/api/";
